b34e778baafdbc025bd2045571c27d7aa27137df
Ghost <168078572+ghostAnalytix@users.noreply.github.com>
2025-01-02 17:28:19 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/study-tracker">
       <div className="min-h-screen bg-white">
         <Header />
         <Routes>
